a52be2ef214a21b67c572a794cb27707bd13f313
Hamza Ahmed <hamzasahmed11@gmail.com>
2022-03-15 10:42:38 -0400

    FB button added

--- TestMeApp/ViewController.swift
+++ TestMeApp/ViewController.swift
@@ -1,19 +1,48 @@
 //
 //  ViewController.swift
-//  TestMeApp
+//  TestMe
 //
-//  Created by admin on 3/14/22.
+//  Created by admin on 3/15/22.
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController , LoginButtonDelegate {
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
     }
+    
+    @IBOutlet weak var lgbtn: FBLoginButton!
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            if let token = AccessToken.current, !token.isExpired{
+                let token = token.tokenString
+                
+                let req = FBSDKLoginKit.GraphRequest(graphPath: "me", parameters: ["fields": "email,name"], tokenString: token, version: nil, httpMethod: .get)
+                req.start{
+                    conne, result, error in
+                    print(result)
+                }
+            } else {
+                //let loginbtn = FBLoginButton()
+                //loginbtn.center = view.center
+                //view.addSubview(loginbtn)
+                self.lgbtn.delegate = self
+                lgbtn.permissions = ["public_public", "email"]
+            }
+        }
 
+        func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+            let token = result?.token?.tokenString
+            
+            let req = FBSDKLoginKit.GraphRequest(graphPath: "me", parameters: ["fields": "email,name"], tokenString: token, version: nil, httpMethod: .get)
+            req.start{
+                conne, result, error in
+                print(result)
+            }
+        }
 
+    @IBAction func loginfb(_ sender: Any) {
+    }
 }
 
